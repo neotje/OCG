04a513c265d98326e6e8710f6f8ea737ae53fce0
neotje <neotje111@gmail.com>
2022-06-04 19:20:44 +0200

    lcd control

---
+++ include/lcd.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Arduino.h>
+#include <DFRobot_RGBLCD1602.h>
+#include <FastLED.h>
+
+DFRobot_RGBLCD1602 lcd(16, 2);
+
+void lcdSetup() {
+    lcd.init();
+    lcd.setColorWhite();
+}
+
+void lcdSetColor(const CRGB &color) {
+    lcd.setRGB(color.r, color.g, color.b);
+}
+
+DFRobot_RGBLCD1602* getLcd() {
+    return &lcd;
+}
