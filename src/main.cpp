7313c12a3645d3a4f8f79c1c5890d2799a144de0
neotje <neotje111@gmail.com>
2022-06-05 09:51:56 +0200

    rotary debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <Arduino.h>
 #include <FastLED.h>
+
 #include "leds.h" 
+#include "ledsMap.h"
+#include "lcd.h"
+#include "rotary.h"
 
 void setup() {
+    Serial.begin(9600);
+
     ledsSetup();
+    lcdSetup();
+    rotarySetup();
+
+    lcdSetColor(CRGB::White);
 }
 
 void loop() {
     ledsLoop();
+    rotaryLoop();
+
+    getLcd()->setCursor(0, 0);
+    getLcd()->print("P: ");
+    getLcd()->print(getRotaryPosition());
+    getLcd()->setCursor(0, 1);
+
+    getLcd()->print("D: ");
+    getLcd()->print(getRotaryDelta());
 }
