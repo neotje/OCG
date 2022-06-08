d1df2a4c7dbfb73dbcdf2bc3405760a120f5ef60
neotje <neotje111@gmail.com>
2022-06-08 22:00:36 +0200

    sd card reader hardware

---
+++ include/sdReader.h
@@ -0,0 +1,19 @@
+#pragma
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+#include "buttons.h"
+
+const byte CS_PIN = 10;
+
+void sdReaderSetup() {
+    while(!SD.begin(CS_PIN)) {
+
+        buttonsLoop();
+
+        if (isButtonLongPressed(0)) {
+            break;
+        }
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,30 @@
 #include "lcd.h"
 #include "rotary.h"
 #include "buttons.h"
+#include "sdReader.h"
 
 #include "menuStateMachine.h"
 
 void setup() {
+    lcdSetup();
+
+    // init message
+    lcdSetColor(CRGB::Red);
+    getLcd()->print("Initializing...");
+
     Serial.begin(9600);
 
+    // hardware
     ledsSetup();
-    lcdSetup();
-    lcdSetColor(CRGB::Gray);
     rotarySetup();
     buttonsSetup();
+    sdReaderSetup();
 
-    ledsFill(CRGB::White);
-
+    // state machines
     menuStateMachineSetup();
+
+    lcdSetColor(CRGB::Gray);
+    getLcd()->clear();
 }
 
 void loop() {
