182028f985f38fc865cdc4f59c3a9b40b2e91797
neotje <neotje111@gmail.com>
2022-06-13 11:12:04 +0200

    removed unnecessary import

--- include/flash.h
+++ include/flash.h
@@ -5,49 +5,12 @@
 #include <SdFat.h>
 #include <Adafruit_SPIFlash.h>
 
-#include "ff.h"
-#include "diskio.h"
-
 #include "pins.h"
 
 Adafruit_FlashTransport_QSPI flashTransport;
 Adafruit_SPIFlash flash(&flashTransport);
 
 FatFileSystem fs;
-
-bool flashFormat() {
-    FATFS fatfs;
-    uint8_t workBuf[4096];
-
-    Serial.println("Formatting flash...");
-
-    FRESULT r = f_mkfs("", FM_FAT | FM_SFD, 0, workBuf, sizeof(workBuf));
-
-    if (r != FR_OK) {
-        Serial.print("Error, f_mkfs failed with error code: "); Serial.println(r, DEC);
-        return false;
-    }
-
-    r = f_mount(&fatfs, "0:", 1);
-    if (r != FR_OK) {
-        Serial.print("Error, f_mount failed with error code: "); Serial.println(r, DEC);
-        return false;
-    }
-
-    r = f_setlabel("");
-    if (r != FR_OK) {
-        Serial.print("Error, f_setlabel failed with error code: "); Serial.println(r, DEC);
-        return false;
-    }
-
-    f_unmount("0:");
-
-    flash.syncBlocks();
-
-    Serial.println("Formatted flash!");
-
-    return true;
-}
 
 void flashSetup() {
     if (!flash.begin()) {
